c3418fb81dbe243249613f2a5b23dcc616a8ff17
Mulyana N <mulyanan.ur26@gmail.com>
2025-07-30 17:08:15 +0700

    Create profile entity

---
+++ lib/profile.dart
@@ -0,0 +1,9 @@
+class Profile {
+  var name = "Mulyana N";
+  var profession = "Flutter Developer";
+  var email = "mulyanan@solecode.id";
+  var phone = "08123456789";
+  var address = "Tangerang, Indonesia";
+  var bio =
+      "Passionate mobile developer with 3+ years experience in Flutter and React Native. Love creating beautiful and functional mobile applications.";
+}
