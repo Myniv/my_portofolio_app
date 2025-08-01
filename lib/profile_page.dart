838b7835e32dc14bcdf7ad8157ddbc818c96d115
Myniv <122717705+Myniv@users.noreply.github.com>
2025-08-01 17:33:21 +0700

    Add card to the profile page

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -13,7 +13,41 @@ class ProfilePage extends StatelessWidget {
             padding: const EdgeInsets.all(16.0),
             child: Column(
               children: [
-                buildProfileHeader(),
+                Container(
+                  padding: const EdgeInsets.all(10.0),
+                  // width: 500,
+                  height: 230,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [Colors.blueAccent, Colors.blueGrey],
+                    ),
+                    // color: const Color.fromARGB(255, 157, 121, 108),
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      // BoxShadow(
+                      //   color: Colors.black26,
+                      //   blurRadius: 10,
+                      //   offset: Offset(0, 5),
+                      // ),
+                    ],
+                  ),
+                  child: Stack(
+                    children: [
+                      Align(
+                        alignment: Alignment.center,
+                        child: buildProfileHeader(),
+                      ),
+                      Positioned(
+                        top: 0,
+                        right: 0,
+                        child: IconButton(
+                          icon: Icon(Icons.edit, color: Colors.white),
+                          onPressed: () {},
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
                 buildProfileInfo(),
                 buildProfileBio(),
               ],
@@ -38,12 +72,16 @@ class ProfilePage extends StatelessWidget {
           SizedBox(height: 10),
           Text(
             profile.name,
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
           ),
           SizedBox(height: 5),
           Text(
             profile.profession,
-            style: TextStyle(fontSize: 16, color: Colors.grey),
+            style: TextStyle(fontSize: 16, color: Colors.white),
           ),
         ],
       ),
@@ -52,33 +90,61 @@ class ProfilePage extends StatelessWidget {
 
   Widget buildProfileInfo() {
     final profile = Profile();
+
     return Padding(
-      padding: const EdgeInsets.all(10.0),
+      padding: const EdgeInsets.only(left: 10.0, right: 10.0, top: 30.0),
       child: Column(
-        // crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Row(
-            children: [
-              Icon(Icons.email, color: Colors.grey),
-              SizedBox(width: 10),
-              Text(profile.email, style: TextStyle(color: Colors.grey)),
-            ],
+          buildInfoBox(Icons.email, profile.email, "Email", () {}),
+          buildInfoBox(Icons.phone, profile.phone, "Phone Number", () {}),
+          buildInfoBox(Icons.location_on, profile.address, "Address", () {}),
+          // buildInfoBox(Icons.info, profile.bio, () {}),
+        ],
+      ),
+    );
+  }
+
+  Widget buildInfoBox(
+    IconData leadingIcon,
+    String text,
+    String title,
+    VoidCallback onEdit,
+  ) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+      margin: EdgeInsets.only(bottom: 10),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.blueAccent),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            alignment: Alignment.center,
+            child: Icon(leadingIcon, color: Colors.blueGrey),
           ),
-          SizedBox(height: 10),
-          Row(
-            children: [
-              Icon(Icons.phone, color: Colors.grey),
-              SizedBox(width: 10),
-              Text(profile.phone, style: TextStyle(color: Colors.grey)),
-            ],
+          SizedBox(width: 10),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  title,
+                  style: TextStyle(
+                    color: Colors.deepPurple,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 4),
+                Text(text, style: TextStyle(color: Colors.blueGrey)),
+              ],
+            ),
           ),
-          SizedBox(height: 10),
-          Row(
-            children: [
-              Icon(Icons.location_on, color: Colors.grey),
-              SizedBox(width: 10),
-              Text(profile.address, style: TextStyle(color: Colors.grey)),
-            ],
+          IconButton(
+            icon: Icon(Icons.edit, color: Colors.blueAccent),
+            onPressed: onEdit,
           ),
         ],
       ),
@@ -87,23 +153,47 @@ class ProfilePage extends StatelessWidget {
 
   Widget buildProfileBio() {
     final profile = Profile();
-
     return Padding(
-      padding: EdgeInsets.all(8.0),
+      padding: const EdgeInsets.only(left: 10.0, right: 10.0),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          SizedBox(height: 5),
-          Padding(padding: EdgeInsets.all(8.0)),
-          Text(
-            "About Me",
-            style: TextStyle(
-              fontSize: 18,
-              color: Colors.grey,
-              fontWeight: FontWeight.bold,
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+            margin: EdgeInsets.only(bottom: 10),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.blueAccent),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        "About Me",
+                        style: TextStyle(
+                          color: Colors.blueGrey,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 4),
+                      Text(
+                        profile.bio,
+                        style: TextStyle(color: Colors.blueGrey),
+                      ),
+                    ],
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.edit, color: Colors.blueAccent),
+                  onPressed: () {},
+                ),
+              ],
             ),
           ),
-          Text(profile.bio, style: TextStyle(color: Colors.grey)),
         ],
       ),
     );
