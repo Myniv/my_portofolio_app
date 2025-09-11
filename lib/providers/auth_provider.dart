86921e09d4a1dc63802fc59f7cb399634d040de9
Myniv <122717705+Myniv@users.noreply.github.com>
2025-09-11 13:39:02 +0700

    Create uid using shared preference to store the user login id

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:my_portofolio_app/providers/profile_provider.dart';
 import 'package:my_portofolio_app/services/auth_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthProvider extends ChangeNotifier {
   final AuthService _authService = AuthService();
@@ -29,6 +30,7 @@ class AuthProvider extends ChangeNotifier {
       _user = await _authService.signInWithEmail(email, password);
 
       if (_user != null) {
+        saveUserUID(_user!.uid);
         await profileProvider.loadProfile(_user!.uid);
         _user = user;
         print("User in auth provider: $_user");
@@ -74,6 +76,7 @@ class AuthProvider extends ChangeNotifier {
       print("User in auth provider: $_user");
 
       if (_user != null) {
+        saveUserUID(_user!.uid);
         await profileProvider.loadProfile(_user!.uid);
         _user = user;
         print("User in auth provider: $_user");
@@ -93,6 +96,30 @@ class AuthProvider extends ChangeNotifier {
   Future<void> signOut() async {
     await _authService.signOut();
     _user = null;
+    clearAllPreferences();
     notifyListeners();
+  }
+
+  Future<void> saveUserUID(String uid) async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setString('uid', uid);
+    } catch (e) {
+      throw Exception('Failed to save user UID: $e');
+    }
+  }
+
+  Future<String?> loadUserUID() async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      return prefs.getString('uid');
+    } catch (e) {
+      throw Exception('Failed to get user UID: $e');
+    }
+  }
+
+  clearAllPreferences() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.clear();
   }
 }
