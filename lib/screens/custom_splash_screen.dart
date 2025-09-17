d72ad2e52af7effdf99636a0253c09b0661327b3
Myniv <122717705+Myniv@users.noreply.github.com>
2025-09-17 23:13:10 +0700

    Update splashscreen

--- lib/screens/custom_splash_screen.dart
+++ lib/screens/custom_splash_screen.dart
@@ -33,7 +33,7 @@ class _CustomSplashScreenState extends State<CustomSplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: Colors.white,
       body: Center(
         child: AnimatedOpacity(
           opacity: _visible ? 1.0 : 0.0,
@@ -46,7 +46,24 @@ class _CustomSplashScreenState extends State<CustomSplashScreen> {
               letterSpacing: 4,
             ),
             duration: Duration(seconds: 2),
-            child: Text("My Portfolio"),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Portofolio App",
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: _visible ? 64 : 20,
+                  ),
+                ),
+                SizedBox(height: 20),
+                AnimatedScale(
+                  scale: _visible ? 1.5 : 0.5,
+                  duration: const Duration(seconds: 2),
+                  child: const CircularProgressIndicator(color: Colors.blue),
+                ),
+              ],
+            ),
           ),
         ),
       ),
